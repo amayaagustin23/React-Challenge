30c93bd385a504492fc86e420644cd68b6db1925
agustin <amayaagustin.2395@gmail.com>
2022-09-12 21:39:33 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,107 +1,97 @@
-import "./index.css";
-import { useState } from "react";
-import ToDoItem from "./components/TodoItem";
-import FilterButton from "./components/FilterButton";
+import './index.css'
+import { useState } from 'react'
+import ToDoItem from './components/TodoItem'
+import FilterButton from './components/FilterButton'
 
 const initialData = [
-  { id: 1, title: "Lorem ipsum dolor sit amet", completed: false },
-  { id: 2, title: "Vivamus id arcu laoreet", completed: false },
-  { id: 3, title: "Donec cursus mi", completed: true },
-  { id: 4, title: "Aenean id fringilla justo", completed: false }
-];
+	{ id: 1, title: 'Lorem ipsum dolor sit amet', completed: false },
+	{ id: 2, title: 'Vivamus id arcu laoreet', completed: false },
+	{ id: 3, title: 'Donec cursus mi', completed: true },
+	{ id: 4, title: 'Aenean id fringilla justo', completed: false },
+]
 
 const ToDo = () => {
-  const [addTodo, setAddTodo] = useState("");
-  const [filter, setFilter] = useState("all");
-  const [todos, setTodos] = useState(initialData);
+	const [addTodo, setAddTodo] = useState('')
+	const [filter, setFilter] = useState('all')
+	const [todos, setTodos] = useState(initialData)
 
-  const handleStatus = (item) => {
-    // marcar como completada o no una tarea
+	const handleStatus = (item) => {
+		const list = []
+		todos.forEach((element) => {
+			if (element.id === item.id) {
+				item.completed = !item.completed
+			}
+			list.push(element)
+		})
+		setTodos(list)
+	}
 
-  };
+	const handleRemove = (item) => {
+		const ItemRemoved = todos.filter((element) => item.id !== element.id)
+		setTodos(ItemRemoved)
+	}
 
-  const handleRemove = (item) => {
-    // eliminar una tarea
+	const handleAddTodo = () => {
+		const newTodo = {
+			id: todos.length + 1,
+			title: addTodo,
+			completed: false,
+		}
+		setTodos([...todos, newTodo])
+		setAddTodo('')
+	}
 
-  };
+	const filterTodos = () => {
+		if (filter === 'complete') {
+			return todos.filter((element) => element.completed === true)
+		} else if (filter === 'incomplete') {
+			return todos.filter((element) => element.completed === false)
+		}
+		return todos
+	}
 
-  const handleAddTodo = () => {
-    // agregar una tarea
-  };
+	const handleChange = (e) => {
+		setAddTodo(e.target.value)
+	}
 
-  const filterTodos = () => {
-    // filtrar tareas por completadas, no completadas, todas.
-    return todos
-  };
+	const visibleTodos = filterTodos()
+	const doneCount = todos.filter((item) => item.completed === true).length
 
-  const handleChange = (e) => {
-    setAddTodo(e.target.value);
-  };
-
-  const visibleTodos = filterTodos();
-  const doneCount = 0
-
-  return (
-    <div>
-      <div className="field">
-        <input value={addTodo} onChange={handleChange} />
-        <button
-          className="btn btn--add"
-          onClick={handleAddTodo}
-          disabled={addTodo.length < 1}
-        >
-          Add
-        </button>
-      </div>
-      <div className="filter-wrapper">
-        <div className="filter-tabs">
-          <FilterButton
-            activeFilter={filter}
-            filter="all"
-            onClick={() => setFilter("all")}
-          />
-          <FilterButton
-            activeFilter={filter}
-            filter="complete"
-            onClick={() => setFilter("complete")}
-          />
-          <FilterButton
-            activeFilter={filter}
-            filter="incomplete"
-            onClick={() => setFilter("incomplete")}
-          />
-        </div>
-        <p style={{ lineHeight: 1.5 }}>
-          {doneCount === todos.length
-            ? `🎉 ${doneCount}/${todos.length} all todos complete!`
-            : `${doneCount}/${todos.length} todos complete`}
-        </p>
-      </div>
-      {visibleTodos.length === 0 && (
-        <p style={{ paddingLeft: "1rem" }}>No todos to show here...</p>
-      )}
-      {visibleTodos.length > 0 &&
-        visibleTodos.map((item, idx) => {
-          return (
-            <ToDoItem
-              key={item.id}
-              item={item}
-              handleStatus={() => handleStatus(item)}
-              handleRemove={() => handleRemove(item)}
-            />
-          );
-        })}
-    </div>
-  );
-};
+	return (
+		<div>
+			<div className='field'>
+				<input placeholder='Add a new task' value={addTodo} onChange={handleChange} />
+				<button className='btn btn--add' onClick={handleAddTodo} disabled={addTodo.length < 1}>
+					Add
+				</button>
+			</div>
+			<div className='filter-wrapper'>
+				<div className='filter-tabs'>
+					<FilterButton activeFilter={filter} filter='all' onClick={() => setFilter('all')} />
+					<FilterButton activeFilter={filter} filter='complete' onClick={() => setFilter('complete')} />
+					<FilterButton activeFilter={filter} filter='incomplete' onClick={() => setFilter('incomplete')} />
+				</div>
+				<p style={{ lineHeight: 1.5 }}>{doneCount === todos.length ? `🎉 ${doneCount}/${todos.length} all todos complete!` : `${doneCount}/${todos.length} todos complete`}</p>
+			</div>
+			{visibleTodos.length === 0 ? (
+				<p style={{ paddingLeft: '1rem' }}>No todos to show here...</p>
+			) : (
+				visibleTodos.length > 0 &&
+				visibleTodos.map((item, idx) => {
+					return <ToDoItem key={idx} item={item} handleStatus={() => handleStatus(item)} handleRemove={() => handleRemove(item)} />
+				})
+			)}
+		</div>
+	)
+}
 
 export default function App() {
-  return (
-    <div className="container">
-      <h1>
-        <strong>ToDo</strong> List
-      </h1>
-      <ToDo />
-    </div>
-  );
+	return (
+		<div className='container'>
+			<h1>
+				<strong>ToDo</strong> List
+			</h1>
+			<ToDo />
+		</div>
+	)
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,50 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from '@testing-library/react'
+import App from './App'
+import ToDoItem from './components/TodoItem'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('Render TodoItem', () => {
+	const item = {
+		id: 5,
+		title: 'Lorem nipsum dolor sit amet',
+		completed: false,
+	}
+	render(<ToDoItem item={item} />)
+	const linkElement = screen.getByText('Lorem nipsum dolor sit amet')
+	expect(linkElement).toBeInTheDocument()
+})
+
+test('create a new todo', () => {
+	render(<App />)
+	const input = screen.getByPlaceholderText(/Add a new task/i)
+	const button = screen.getByRole('button', { name: /Add/i })
+	fireEvent.change(input, { target: { value: 'new TODO' } })
+	fireEvent.click(button)
+	fireEvent.change(input, { target: { value: '' } })
+	const existToDo = screen.getByText(/new TODO/i)
+	expect(existToDo).not.toBeNull()
+	expect(input.value).toBe('')
+})
+
+test('remove a todo', () => {
+	render(<App />)
+	const buttonsRemove = screen.getAllByRole('button', { name: '' })
+	fireEvent.click(buttonsRemove[1])
+	const buttonsRemoved = screen.getAllByRole('button', { name: '' })
+	expect(buttonsRemoved.length < buttonsRemove.length).toEqual(true)
+})
+
+test('checked a todo', () => {
+	render(<App />)
+	const buttonChecked = screen.getByRole('button', { name: 'Lorem ipsum dolor sit amet' })
+	fireEvent.click(buttonChecked)
+  expect(buttonChecked).toHaveStyle("text-decoration: line-through")
+})
+
+
+test('filter completed', () => {
+	render(<App />)
+	const toDoIncomplete = screen.getByRole('button', { name: 'Lorem ipsum dolor sit amet' })
+	const buttonIncomplete = screen.getByRole('button', { name: 'incomplete' })
+	fireEvent.click(buttonIncomplete)
+  expect(toDoIncomplete!==undefined).toEqual(true)
+})
